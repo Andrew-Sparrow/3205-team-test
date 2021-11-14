5fadfaacddefa972b05cb4dc90549da7999c912d
Andrew <8911102@gmail.com>
2021-11-14 21:34:56 +0500

    mui input was added

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,12 +1,13 @@
 import {FC} from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
 
 interface LayoutProps {
   className: string;
 }
 
-const Layout: FC<LayoutProps> = (props) =>{
+const Layout: FC<LayoutProps> = (props) => {
   const {
     children,
     className
@@ -16,15 +17,14 @@ const Layout: FC<LayoutProps> = (props) =>{
     <div className={className}>
       <header className="header">
         <div className="container">
-          <div className="header__wrapper">
-            <h2 className="header__title">Currency</h2>
-            <Stack spacing={2} direction="row">
-              <Button variant="contained" size="large">Currency Converter</Button>
-              <Button variant="contained" size="large">List of Exchange Rates</Button>
-            </Stack>
-          </div>
+          <h2 className="header__title">Currency Converter</h2>
+          <Stack spacing={2} direction="row" justifyContent="space-between" mb={5}>
+            <Button variant="contained" size="large">Currency Converter</Button>
+            <Button variant="contained" size="large">List of Exchange Rates</Button>
+          </Stack>
         </div>
       </header>
+      <Divider />
       {children}
     </ div>
   );
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,9 +1,21 @@
 import {FC} from 'react';
 import withLayout from '../hocs/with-layout';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 const Main: FC = () => {
   return (
-    <p className="page page--gray page--index"></p>
+    <div className="container">
+      <Stack spacing={2} direction="row" justifyContent="space-between" mt={5}>
+        <TextField
+          id="outlined-read-only-input"
+          label="Enter Currency Amount By Template"
+          defaultValue="15 usd in rub"
+        />
+        <Button variant="contained" size="large">Calculate</Button>
+      </Stack>
+    </div>
   );
 };
 
