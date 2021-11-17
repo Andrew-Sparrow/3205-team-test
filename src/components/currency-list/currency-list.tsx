90fbe143a464d3460cf69da2a40b34e68e294c78
Andrew <8911102@gmail.com>
2021-11-17 14:17:18 +0500

    ability to switch out buttons after browser history navigation was added

--- src/components/currency-list/currency-list.tsx
+++ src/components/currency-list/currency-list.tsx
@@ -1,9 +1,32 @@
-import {FC} from 'react';
+import {FC, useEffect} from 'react';
+import withLayout from '../hocs/with-layout';
+import {useDispatch} from 'react-redux';
+import {useLocation} from 'react-router-dom';
+import {activeTabNames} from '../../const';
+import {changeTab} from '../../store/actions';
+import {AppRoute} from '../../const';
+
 
 const CurrencyList: FC = () => {
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  // this was made to synchronize tabs state with browser history tabs
+  useEffect(() => {
+    switch (location.pathname) {
+      case AppRoute.MAIN:
+        dispatch(changeTab(activeTabNames.CONVERTER));
+        break;
+      case AppRoute.CURRENCY_LIST:
+        dispatch(changeTab(activeTabNames.CURRENCY_LIST));
+        break;
+    }
+  }, [location, dispatch]);
+
   return (
     <p>wwwwwwww</p>
   );
 };
 
-export default CurrencyList;
+const withLayoutMain = withLayout(CurrencyList);
+export default withLayoutMain;
